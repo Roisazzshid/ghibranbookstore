75dd6aa2e2e3a9053f93669e1ffe7165a333d59e
Roisazzshid3 <isamiq3@gmail.com>
2026-05-16 14:16:35 +0700

    Improve navbar layout and enhance user experience with updated links and styling

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,91 +4,145 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 ?>
 
-<nav class="navbar navbar-expand-lg bg-white shadow-sm">
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm sticky-top">
+
     <div class="container">
 
+        <!-- Logo -->
         <a class="navbar-brand fw-bold text-primary"
         href="index.php">
-            GhibranBookStore
+
+            📚 GhibranBookStore
+
         </a>
 
-        <button class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav">
+        <!-- Toggle Mobile -->
+        <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav">
 
             <span class="navbar-toggler-icon"></span>
+
         </button>
 
-        <div class="collapse navbar-collapse"
+        <!-- Menu -->
+        <div
+        class="collapse navbar-collapse"
         id="navbarNav">
 
-            <ul class="navbar-nav ms-auto">
+            <ul class="navbar-nav ms-auto align-items-lg-center gap-lg-2">
 
                 <li class="nav-item">
                     <a class="nav-link"
                     href="index.php">
-                    Home
-                    </a>
-                </li>
 
-                <li class="nav-item">
-                    <a class="nav-link"
-                    href="about.php">
-                    About
-                    </a>
-                </li>
+                        Home
 
-                <li class="nav-item">
-                    <a class="nav-link"
-                    href="contact.php">
-                    Contact
                     </a>
                 </li>
 
                 <li class="nav-item">
                     <a class="nav-link"
                     href="books.php">
-                    Buku
+
+                        Buku
+
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link"
+                    href="about.php">
+
+                        About Us
+
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link"
+                    href="contact.php">
+
+                        Contact
+
                     </a>
                 </li>
 
                 <?php if(isset($_SESSION['user'])) : ?>
 
-                    <li class="nav-item">
-                        <span class="nav-link">
-                            Hi,
-                            <?= $_SESSION['user']['fullname']; ?>
-                        </span>
-                    </li>
+                <li class="nav-item">
+                    <a class="nav-link"
+                    href="cart.php">
 
-                    <li class="nav-item">
-                        <a class="btn btn-danger ms-2"
-                        href="logout.php">
-                            Logout
-                        </a>
-                    </li>
+                        🛒 Keranjang
+
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link"
+                    href="status.php">
+
+                        Status Pesanan
+
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <span class="nav-link fw-semibold text-primary">
+
+                        Hi,
+                        <?= $_SESSION['user']['fullname']; ?>
+
+                    </span>
+                </li>
+
+                <li class="nav-item">
+
+                    <a
+                    class="btn btn-danger"
+                    href="logout.php">
+
+                        Logout
+
+                    </a>
+
+                </li>
 
                 <?php else : ?>
 
-                    <li class="nav-item">
-                        <a class="btn btn-outline-primary ms-2"
-                        href="register.php">
-                            Register
-                        </a>
-                    </li>
+                <li class="nav-item">
 
-                    <li class="nav-item">
-                        <a class="btn btn-primary ms-2"
-                        href="login.php">
-                            Login
-                        </a>
-                    </li>
+                    <a
+                    class="btn btn-outline-primary"
+                    href="register.php">
+
+                        Register
+
+                    </a>
+
+                </li>
+
+                <li class="nav-item">
+
+                    <a
+                    class="btn btn-primary"
+                    href="login.php">
+
+                        Login
+
+                    </a>
+
+                </li>
 
                 <?php endif; ?>
 
             </ul>
 
         </div>
+
     </div>
+
 </nav>
